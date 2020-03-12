e31614ccc4c5563db781cbd2f0df08c745f443fc
zhaojun <1048317016@qq.com>
2020-03-12 11:55:28 +0800

    调整项目结构

---
+++ js/footer.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+(function () {
+    //倪静楠-js
+    let objs = document.getElementsByClassName('njn-mouse');
+    for (let i = 0; i < objs.length; i++) {
+        objs[i].onmouseover = function () {
+            this.classList.add('njn-mouse-on');
+        };
+    }
+    for (let i = 0; i < objs.length;i++) {
+        objs[i].onmouseout = function () {
+            this.classList.remove('njn-mouse-on');
+        }
+    };
+    // 
+    // 
+    // 
+    // 周昭君 -js
+    let foot = document.getElementById('brand-link')
+    let text = document.getElementById('zzj-text')
+
+    function addHoveEvent(parent) {
+        var opation = parent.getElementsByTagName("a");
+        console.log(opation)
+        for (let i = 0; i < opation.length; i++) {
+            opation[i].onmouseover = function () {
+
+                let siblings = this.parentNode.children;
+                let id = this.parentNode.index;// 0--3
+                for (let j = 0; j < siblings.length; j++) {
+                    siblings[j].style.color = "";
+                    siblings[j].style.borderBottom = '';
+                }
+                this.style.color = "red";
+                this.style.borderBottom = '1px solid red'
+            }
+
+            opation[i].onmouseleave = function () {
+                this.style.color = "";
+                this.style.borderBottom = ''
+            }
+        }
+    }
+
+    addHoveEvent(foot)
+    addHoveEvent(text)
+}())
--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-
-(function () {
-    let foot = document.getElementById('brand-link')
-    let text = document.getElementById('zzj-text')
-
-    function addHoveEvent(parent) {
-        var opation = parent.getElementsByTagName("a");
-        console.log(opation)
-        for (let i = 0; i < opation.length; i++) {
-            opation[i].onmouseover = function () {
-
-                let siblings = this.parentNode.children;
-                let id = this.parentNode.index;// 0--3
-                for (let j = 0; j < siblings.length; j++) {
-                    siblings[j].style.color = "";
-                    siblings[j].style.borderBottom = '';
-                }
-                this.style.color = "red";
-                this.style.borderBottom = '1px solid red'
-            }
-
-            opation[i].onmouseleave = function () {
-                this.style.color = "";
-                this.style.borderBottom = ''
-            }
-        }
-    }
-
-    addHoveEvent(foot)
-    addHoveEvent(text)
-}())
+$("#main-footer").load('footer.html', function() {
+    $.getScript('./js/footer.js')
+})
